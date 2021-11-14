6d2bbc64261a9967886d885f22a3be0af9c9408c
praveenyadav61 <mail2pky@gmail.com>
2021-11-14 13:48:23 +0530

    added main.py

--- IP_codes/main.py
+++ IP_codes/main.py
@@ -1,36 +1,44 @@
 import cv2
+import os
 
 print(os.getcwd())
 
 # Folder which contains all the images
 # from which video is to be generated
 os.chdir("D:\\Hackathons\\HackFit\\Media\\yoga_poses")
-path = "D:\\Hackathons\\HackFit\\Media\\yoga_poses"
-
-images = [img for img in os.listdir(image_folder)
-              if img.endswith(".jpg") or
-                 img.endswith(".jpeg") or
-                 img.endswith("png")]
-
-for im in images:
-    cv.imshow(im)
+folder = "D:\\Hackathons\\HackFit\\Media\\yoga_poses"
 
 
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        print(filename)
+        if img is not None:
+            images.append(img)
+    return images
 
-# define a video capture object
+
+images = load_images_from_folder(folder)
+total_cnt = len(images)
+cnt = 0
+
 vid = cv2.VideoCapture(0)
 
 while (True):
-
     # Capture the video frame
     ret, frame = vid.read()
-
     # Display the resulting frame
     cv2.imshow('frame', frame)
+    cv2.imshow('tutorial', images[cnt])
+    if check_matching(frame, images[cnt]):
+        print("matched.........")
+        cnt += 1
+        if cnt == total_cnt:
+            print("completed ")
+            break
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
@@ -38,3 +46,4 @@ while (True):
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+
